b2ab07e20bf1ac526738e421d70d07eb1f0d9257
Truman <trumanngny@yahoo.com>
2021-06-05 23:21:19 -0400

    add code for addColumn and addRow

--- index.js
+++ index.js
@@ -2,11 +2,30 @@ var row = 0;
 var column = 0;
 
 document.getElementById("addRow").addEventListener("click",function(){
-    alert("dwdw");
+    //for the 1st time
+    if(row == 0 & column ==0)
+    {
+        row =1;
+        column =1;
+        let newRow = document.createElement("tr");
+        let newColumn = document.createElement("td");
+        document.getElementById("grid").appendChild(newRow).setAttribute('id',"row"+ row.toString());
+        document.getElementById("row1").appendChild(newColumn).setAttribute('id',"colum"+ column.toString());
+    }
+    
 });
 
 document.getElementById("addColumn").addEventListener("click",function(){
-    alert("dwdw");
+    //for the 1st time
+    if(row == 0 & column ==0)
+    {
+        row =1;
+        column =1;
+        let newRow = document.createElement("tr");
+        let newColumn = document.createElement("td");
+        document.getElementById("grid").appendChild(newRow).setAttribute('id',"row"+ row.toString());
+        document.getElementById("row1").appendChild(newColumn).setAttribute('id',"colum"+ column.toString());
+    }
 });
 
 document.getElementById("delRow").addEventListener("click",function(){
