0b0ee3274f1f4db5c31df4419cb747c9ad005fc3
Truman <trumanngny@yahoo.com>
2021-06-06 00:17:35 -0400

    finish addColumn button and issue #5

--- index.js
+++ index.js
@@ -26,14 +26,22 @@ document.getElementById("addRow").addEventListener("click",function(){
 
 document.getElementById("addColumn").addEventListener("click",function(){
     //for the 1st time
-    let newRow = document.createElement("tr");
-    let newColumn = document.createElement("td");
+    
     if(row == 0 & column ==0)
     {
+        let newRow = document.createElement("tr");
+        let newColumn = document.createElement("td");
         row =1;
         column =1;
         document.getElementById("grid").appendChild(newRow).setAttribute('id',"row"+ row.toString());
         document.getElementById("row1").appendChild(newColumn).setAttribute('id',"colum"+ column.toString());
+    }else
+    {
+        column++;
+        for(let i =1; i <=row; i++)
+        {   let newColumn = document.createElement("td");
+            document.getElementById("row" +i.toString()).appendChild(newColumn);
+        }
     }
 });
 
@@ -54,5 +62,10 @@ document.getElementById("fillAll").addEventListener("click",function(){
 });
 
 document.getElementById("resetColor").addEventListener("click",function(){
-    alert("dwdw");
+    
 });
+
+function colorSelector()
+{
+
+}
