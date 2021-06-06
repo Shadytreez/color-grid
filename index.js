4ea75976428e58c75682002095047e84b4a1c291
Truman <trumanngny@yahoo.com>
2021-06-05 21:55:16 -0400

    add eventListener to all button

--- index.js
+++ index.js
@@ -4,3 +4,27 @@ var column = 0;
 document.getElementById("addRow").addEventListener("click",function(){
     alert("dwdw");
 });
+
+document.getElementById("addColumn").addEventListener("click",function(){
+    alert("dwdw");
+});
+
+document.getElementById("delRow").addEventListener("click",function(){
+    alert("dwdw");
+});
+
+document.getElementById("delColumn").addEventListener("click",function(){
+    alert("dwdw");
+});
+
+document.getElementById("fillEmpt").addEventListener("click",function(){
+    alert("dwdw");
+});
+
+document.getElementById("fillAll").addEventListener("click",function(){
+    alert("dwdw");
+});
+
+document.getElementById("resetColor").addEventListener("click",function(){
+    alert("dwdw");
+});
